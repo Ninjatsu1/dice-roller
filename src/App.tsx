dee8f427d78a1bc67885dd3cdd730123562d63c3
Nina Mäkinen <sherlocked1223@gmail.com>
2020-11-25 13:22:15 +0200

    Added inputs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ function App() {
     <div className="App">
       <header>
         <p>Dice roller</p>
+        <input placeholder="d40" />
+        <input placeholder="d60" />
+        <input placeholder="d80" />
+        <input placeholder="d10" />
+        <input placeholder="d12" />
+        <input placeholder="d20" />
+        <input placeholder="d100" />
       </header>
     </div>
   );
